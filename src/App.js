e66a89c125f867b125852cbf883c784055398922
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-29 11:00:41 +0200

    created example for proptype usage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Greeting person={{ name: "Some one", location: "Somewhere" }} />
   );
 }
 
 export default App;
+
+class Greeting extends React.Component {
+  render() {
+    return <p>Hello {this.props.person.name} in {this.props.person.location}</p>
+  }
+}
+
+Greeting.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string
+  })
+}
+
+Greeting.defaultProps = {
+  person: {
+    name: "Unknown User",
+    location: "Nowhere"
+  }
+}
